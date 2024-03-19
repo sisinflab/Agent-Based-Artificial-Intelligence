5efab05f88e01fa8134f2ccd8976c2af9f835625
vincenzo <vincenzo.paparella@outlook.it>
2024-03-19 15:05:46 +0100

    treating environments in different script to import

--- main.py
+++ main.py
@@ -2,53 +2,10 @@ from search.Problems import StreetProblem
 from search.strategies import *
 from search.TreeSearch import TreeSearch
 from search.GraphSearch import GraphSearch
+from search.Environments import *
 import math
 
 # model and load the environment
-streets = {
-    'Andria': ['Corato', 'Trani'],
-    'Corato': ['Ruvo', 'Trani', 'Andria', 'Altamura'],
-    'Altamura': ['Corato', 'Ruvo', 'Modugno'],
-    'Ruvo': ['Corato', 'Bisceglie', 'Terlizzi', 'Altamura'],
-    'Terlizzi': ['Ruvo', 'Molfetta', 'Bitonto'],
-    'Bisceglie': ['Trani', 'Ruvo', 'Molfetta'],
-    'Trani': ['Andria', 'Corato', 'Bisceglie'],
-    'Molfetta': ['Bisceglie', 'Giovinazzo', 'Terlizzi'],
-    'Giovinazzo': ['Molfetta', 'Modugno', 'Bari', 'Bitonto'],
-    'Bitonto': [ 'Modugno', 'Giovinazzo', 'Terlizzi'],
-    'Modugno': ['Bitonto', 'Giovinazzo', 'Bari', 'Altamura'],
-    'Bari': ['Modugno', 'Giovinazzo']
-}
-
-# cities coordinates
-streets_coords = {
-    'Andria': (41.2316, 16.2917),
-    'Corato': (41.1465, 16.4147),
-    'Altamura': (40.8302, 16.5545),
-    'Ruvo': (41.1146, 16.4886),
-    'Terlizzi': (41.1321, 16.5461),
-    'Bisceglie': (41.243, 16.5052),
-    'Trani': (41.2737, 16.4162),
-    'Molfetta': (41.2012, 16.5983),
-    'Giovinazzo': (41.1874, 16.6682),
-    'Bitonto': (41.1118, 16.6902),
-    'Modugno': (41.0984, 16.7788),
-    'Bari': (41.1187, 16.852)
-}
-
-
-class Roads:
-    def __init__(self, streets, coordinates):
-        self.streets = streets
-        self.coordinates = coordinates
-
-    def distance(self, start, end):
-        lat_a, long_a = self.coordinates[start]
-        lat_b, long_b = self.coordinates[end]
-        lat_diff = abs(lat_a - lat_b)*111  # <- *111 to just convert the latitude distance in KM.
-        long_diff = abs(long_a - long_b)*111 # <- *111 to just convert the longitude distance in KM.
-        return math.sqrt(lat_diff**2 + long_diff**2)
-
 
 map = Roads(streets, streets_coords)
 # formulate the problem
